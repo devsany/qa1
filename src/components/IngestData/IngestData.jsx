26236aab00d8c0d01c677f7eb104544b0bcc2fa4
devsany <sunnyrajbodhgaya19@gmail.com>
2025-02-05 16:57:00 +0530

    second-ingest

--- src/components/IngestData/IngestData.jsx
+++ src/components/IngestData/IngestData.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import First from "./first/First";
+import Second from "./second/Second";
 
 const IngestData = () => {
   return (
     <div>
       {/* Upper component */}
       <First />
+      <Second />
     </div>
   );
 };
